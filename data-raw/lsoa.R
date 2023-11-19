281b7ee54f51e80f8e2e290004edae64ce858537
Roberto Villegas-Diaz <bthillo@hotmail.com>
2023-11-19 18:00:43 +0000

    Update code to extract LSOA subsets for other years (new version needed for subsets prior to 2019)

--- data-raw/lsoa.R
+++ data-raw/lsoa.R
@@ -1,78 +1,117 @@
 # Load packages ----
-library(readxl)
-library(tidyverse)
-library(here)
-library(janitor)
+# library(readxl)
+# library(tidyverse)
+# library(here)
+# library(janitor)
 
-# URL to zipped data
-# url_lsoa <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2flowersuperoutputareamidyearpopulationestimates%2fmid2019sape22dt2/sape22dt2mid2019lsoasyoaestimatesunformatted.zip"
-# dont know how to unzip without saving file, so just downloading it and adding it to the folder, alltho this is really large and annoying
+# Source -----
+
+# https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates
+# Helper functions -----
+
+tidy_lsoa_data <- function(data,
+                           la_year,
+                           lsoa_year,
+                           est_year,
+                           genre) {
+  data %>%
+    # pivot the wide data into long format
+    tidyr::pivot_longer(cols = dplyr::starts_with("x"),
+                        names_to = "age",
+                        values_to = "n") %>%
+    # update ages to keep numbers only
+    dplyr::mutate(age = as.numeric(gsub("\\D", "", age))) %>%
+    # select a subset of the variables
+    dplyr::select(lsoa_code,
+                  lsoa_name,
+                  la_code = paste0("la_code_", la_year, "_boundaries"),
+                  la_name = paste0("la_name_", la_year, "_boundaries"),
+                  age,
+                  n) %>%
+    # insert variables of the LA, LSOA and estimation year:
+    dplyr::mutate(lsoa_year = lsoa_year,
+                  la_year = la_year,
+                  est_year = est_year,
+                  gender = genre) %>%
+    dplyr::relocate(lsoa_year,
+                    lsoa_code,
+                    lsoa_name,
+                    la_year,
+                    la_code,
+                    la_name,
+                    age,
+                    gender,
+                    est_year,
+                    n)
+}
+
+load_data_frames <- function(URL,
+                             la_year,
+                             lsoa_year = la_year,
+                             est_year = la_year,
+                             dl = tempfile("lsoa_pop", tempdir()),
+                             skip = 4,
+                             zip = TRUE) {
+  # download dataset
+  download.file(URL, dl, mode = "wb")
+  # set up output directory
+  out_dir <- paste0(tempdir(), "//lsoa_pop")
+  if (!dir.exists(out_dir))
+    dir.create(out_dir, recursive = TRUE)
+  if (zip) {
+    # unzip dataset
+    utils::unzip(dl, junkpaths = TRUE, exdir = out_dir)
+    # list contents
+    files <- dir(out_dir, full.names = TRUE, pattern = "xlsx$")
+  } else {
+    files <- dl
+  }
+
+  # list sheets
+  lsoa_pop_sheets <- readxl::excel_sheets(files)
+
+  # estimates for female
+  df_raw_f <- readxl::read_excel(
+    files,
+    sheet = lsoa_pop_sheets[grepl("Females", lsoa_pop_sheets)],
+    skip = skip
+  ) %>%
+    janitor::clean_names() %>% # clean variable names
+    tidy_lsoa_data(la_year, lsoa_year, est_year, "f")
+
+  # estimates for male
+  df_raw_m <- readxl::read_excel(
+    files,
+    sheet = lsoa_pop_sheets[grepl("Males", lsoa_pop_sheets)],
+    skip = skip
+  ) %>%
+    janitor::clean_names() %>% # clean variable names
+    tidy_lsoa_data(la_year, lsoa_year, est_year, "m")
+
+  # join data and arrange by code
+  lsoa <- df_raw_f %>%
+    dplyr::bind_rows(df_raw_m) %>%
+    dplyr::arrange(lsoa_year, lsoa_code, est_year, gender)
+
+  # delete temporary files
+  unlink(dl, recursive = TRUE)
+  unlink(out_dir, recursive = TRUE)
+
+  return(lsoa)
+}
 
 # LSOA 2019 scores ----
-## Estimates for male
-df_raw_m <- read_excel(here("data-raw/SAPE22DT2-mid-2019-lsoa-syoa-estimates-unformatted.xlsx"),
-                       sheet = "Mid-2019 Males",
-                       range = "A5:CT34758")
+lsoa_2019 <- load_data_frames("https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2019sape22dt2/sape22dt2mid2019lsoasyoaestimatesunformatted.zip",
+                              la_year = 2019,
+                              lsoa_year = 2019,
+                              est_year = 2019)
+# LSOA 2020 scores ----
+lsoa_2020 <- load_data_frames("https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2020sape23dt2/sape23dt2mid2020lsoasyoaestimatesunformatted.xlsx",
+                              la_year = 2018,
+                              lsoa_year = 2020,
+                              est_year = 2020,
+                              zip = FALSE)
 
-## Estimates for female
-df_raw_f <- read_excel(here("data-raw/SAPE22DT2-mid-2019-lsoa-syoa-estimates-unformatted.xlsx"),
-                       sheet = "Mid-2019 Females",
-                       range = "A5:CT34758")
-
-# Create vector with better names
-rename_age_vars <- setNames(paste0("x", 0:90),
-                            c(paste0("age", 0:89), "age90plus"))
-
-# Rename both data objects
-df_f_wide <- df_raw_f %>%
-  clean_names() %>%
-  rename(all_of(rename_age_vars))
-
-df_m_wide <- df_raw_m %>%
-  clean_names() %>%
-  rename(all_of(rename_age_vars))
-
-# Pivot data longer
-df_f_long <- df_f_wide %>%
-  pivot_longer(cols = all_of(c(paste0("age", 0:89), "age90plus")),
-               names_to = "age",
-               values_to = "n") %>%
-  mutate(age = as.numeric(str_extract(age, "\\d+"))) %>%
-  rename(n_all_ages_female = all_ages) %>%
-  select(lsoa_code,
-         lsoa_name,
-         la_code = la_code_2019_boundaries,
-         la_name = la_name_2019_boundaries,
-         age,
-         n) %>%
-  mutate(lsoa_year = 2019,
-         la_year = 2019,
-         est_year = 2019,
-         gender = "f") %>%
-  relocate(lsoa_year, lsoa_code, lsoa_name, la_year, la_code, la_name, age, gender, est_year, n)
-
-df_m_long <- df_m_wide %>%
-  pivot_longer(cols = all_of(c(paste0("age", 0:89), "age90plus")),
-               names_to = "age",
-               values_to = "n") %>%
-  mutate(age = as.numeric(str_extract(age, "\\d+"))) %>%
-  rename(n_all_ages_male = all_ages) %>%
-  select(lsoa_code,
-         lsoa_name,
-         la_code = la_code_2019_boundaries,
-         la_name = la_name_2019_boundaries,
-         age,
-         n) %>%
-  mutate(lsoa_year = 2019,
-         la_year = 2019,
-         est_year = 2019,
-         gender = "m") %>%
-  relocate(lsoa_year, lsoa_code, lsoa_name, la_year, la_code, la_name, age, gender, est_year, n)
-
-# Join data and arrange by code
-lsoa <- df_f_long %>%
-  add_row(df_m_long) %>%
-  arrange(lsoa_year, lsoa_code, est_year, gender)
-
+lsoa <- lsoa_2019 # original version (as of 2023-11-19)
 usethis::use_data(lsoa, overwrite = TRUE)
 
